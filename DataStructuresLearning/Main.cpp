87ca81f2b074bda0d49604bd2a73ba0d2e7c2ef0
Ilisoi Fineas <fineasilisoi1@gmail.com>
2026-02-17 21:35:05 +0200

    bubble sort

--- DataStructuresLearning/Main.cpp
+++ DataStructuresLearning/Main.cpp
@@ -6,19 +6,78 @@
 
 using namespace std;
 
+void manual_elements(int& nrOfelements, vector<int>& Nums)
+{
+	cout << "You have selected option 1" << endl;
+	cout << "Enter # of elements: ";
+	cin >> nrOfelements;
+	Nums.resize(nrOfelements);
+	for (int i = 0; i < nrOfelements; i++)
+	{
+		cout << "Enter element [" << i + 1 << "]: ";
+		cin >> Nums[i];
+	}
+}
+
+void random_elements(int& nrOfelements, vector<int>& Nums)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> distInt(1, 10);
+	nrOfelements = 10;
+	Nums.resize(nrOfelements);
+	cout << "Random integers numbers: ";
+	for (int i = 0; i < nrOfelements; ++i) {
+		Nums[i] = distInt(gen);
+		cout << Nums[i] << " ";
+	}
+}
+
+void afisare_elemente(const vector<int>& Nums)
+{
+	cout << "You have selected option 3" << endl;
+	cout << "Numbers: [";
+	for (int i = 0; i < Nums.size(); i++)
+	{
+		cout << Nums[i];
+		if (Nums.size() - 1 != i) {
+			cout << ", ";
+		}
+	}
+	cout << "]" << endl;
+}
+
+void bubble_sort(vector<int>& Nums)
+{
+	for (int i = 0; i < Nums.size() - 1; i++)
+	{
+		for (int j = 0; j < Nums.size() - i - 1; j++)
+		{
+			if (Nums[j] > Nums[j + 1])
+			{
+				swap(Nums[j], Nums[j + 1]);
+			}
+		}
+	}
+}
+
 int main()
 {
 	bool isRunning = true;
 	int option;
 	int nrOfelements;
 	vector<int> Nums;
-	cout << "ILISOI";
 	cout << "------------------------------------" << endl;
 	cout << "---------------MENU-----------------" << endl;
 	cout << "1. Enter Numbers Manually" << endl;
 	cout << "2. Enter Random Numbers" << endl;
 	cout << "3. Display Numbers" << endl;
-	cout << "4. Exit" << endl;
+	cout << "------------------------------------" << endl;
+	cout << "----------SORTING ALGORITHMS--------" << endl;
+	cout << "4. Bubble Sort" << endl;
+
+	cout << "\n\n-------------------------------------" << endl;
+	cout << "0. Exit" << endl;
 	
 
 	while (isRunning) {
@@ -27,51 +86,31 @@ int main()
 		switch (option)
 		{
 		case 1: {
-			cout << "You have selected option 1" << endl;
-			cout << "Enter # of elements: ";
-			cin >> nrOfelements;
-
-			Nums.resize(nrOfelements);
-			for (int i = 0; i < nrOfelements; i++)
-			{
-				cout << "Enter element [" << i + 1 << "]: ";
-				cin >> Nums[i];
-			}
+			manual_elements(nrOfelements, Nums);
 			cout << endl;
 			break;
 		}
 
 		case 2: {
-			random_device rd;
-			mt19937 gen(rd());
-			uniform_int_distribution<int> distInt(1, 10);
-			nrOfelements = 10;
-			Nums.resize(nrOfelements);
-			cout << "Random integers numbers: ";
-			for (int i = 0; i < nrOfelements; ++i) {
-				Nums[i] = distInt(gen);
-				cout << Nums[i]<< " ";
-			}
+			random_elements(nrOfelements, Nums);
 			cout << endl;
 			break;
 		}
 
 		case 3: {
-			cout << "You have selected option 3" << endl;
-			cout << "Numbers: [";
-			for (int i = 0; i < Nums.size(); i++)
-			{
-				cout << Nums[i];
-				if(Nums.size() -1 != i) {
-					cout << ", ";
-				}
-			}
-			cout << "]" << endl;
+			afisare_elemente(Nums);
 			cout << endl;
 			break;
 		}
 
 		case 4: {
+			bubble_sort(Nums);
+			cout << "S-a efectuat sortarea cu Bubble Sort" << endl;
+			cout << endl;
+			break;
+		}
+
+		case 0: {
 			cout << "Exiting the program..." << endl;
 			isRunning = false;
 			break;
